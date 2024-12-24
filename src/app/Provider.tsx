a7227fdf47bee8c505ede88f70abdb2a1d95e1e5
GulshanJha00 <gk4763549@gmail.com>
2024-12-24 23:44:20 +0530

    signin sign up added

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,48 +1,41 @@
-'use client'
-import db from '@/config/db';
-import { Users } from '@/config/schema';
-import { useUser } from '@clerk/nextjs';
-import { eq } from 'drizzle-orm';
-import React, { useEffect } from 'react'
+"use client";
+import db from "@/config/db";
+import { Users } from "@/config/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
 
 const Provider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-
-    const {user} = useUser();
-    useEffect(()=>{
-        if (user) {
-            isNewUser();
-        }
-    }, [user])
-
-    const isNewUser = async () => {
-        const email = user?.primaryEmailAddress?.emailAddress
-        const names = user?.fullName
-        if(!names){
-            console.error("Name is undefined");
-            return;
-        }
-        if (!email) {
-            console.error("User email is undefined");
-            return;
-        }
-        const result = await db.select().from(Users).where(eq(Users.email, email));
-
-        if(!result.length){
-            await db.insert(Users).values({
-                name: names,
-                email: email,
-                imageUrl: user?.imageUrl
-            })
-        }
-
+  const { user } = useUser();
+  useEffect(() => {
+    if (user) {
+      isNewUser();
     }
+  }, [user]);
 
-    return(
+  const isNewUser = async () => {
+    const email = user?.primaryEmailAddress?.emailAddress;
+    const names = user?.fullName;
+    if (!names) {
+      console.error("Name is undefined");
+      return;
+    }
+    if (!email) {
+      console.error("User email is undefined");
+      return;
+    }
+    const result = await db.select().from(Users).where(eq(Users.email, email));
 
-        <div>{children}</div>
-
-    ) 
+    if (!result.length) {
+      await db.insert(Users).values({
+        name: names,
+        email: email,
+        imageUrl: user?.imageUrl,
+      });
+    }
   };
-  
 
-export default Provider
+  return <div>{children}</div>;
+};
+
+export default Provider;
